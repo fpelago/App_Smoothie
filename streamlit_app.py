67fecece22464ad91b288786415329bf68d729c1
fpelago <f.pelago@gmail.com>
2025-08-02 00:16:50 +0200

    Update streamlit_app.py
    insert new section to dispaly smoothiefroot nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,11 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
+#new section to dispaly smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
 # Write directly to the app
 st.title(f":balloon: My Order App :cup_with_straw: ")
 st.write(
