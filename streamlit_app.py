b8092bb9cc9ebc7ae869e552470ddf07dac4d450
fpelago <f.pelago@gmail.com>
2025-08-02 00:34:52 +0200

    Update streamlit_app.py
    replaced text with dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,8 @@ ingredients_list = st.multiselect(
 #new section to dispaly smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 if ingredients_list:
 #    st.write("Hai selezionato:", ingredients_list)
