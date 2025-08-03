78ef47ceec887718c179554216f4b227c28bba0a
fpelago <f.pelago@gmail.com>
2025-08-04 00:53:20 +0200

    Update streamlit_app.py
    add apici alla colonnaSEARCH_ON riga 21

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,9 @@ st.write('The name on your smoothie is: ', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()   # get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col(SEARCH_ON))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_with=True)
-st.stop
+#st.stop
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
