68af271f67630c037b46c483b27aa13f1c15f14e
fpelago <f.pelago@gmail.com>
2025-08-04 01:08:19 +0200

    Update streamlit_app.py
    tolto l'ultimo stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ if ingredients_list:
         sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #    st.write(ingredients_string)
-    st.stop
+    #st.stop
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
